30dae6cb0a2d1b8e70c569d0914d41e3ffd6af67
Алексей Зорин <zorinalexey59292@gmail.com>
2023-01-06 00:22:55 +0400

    start

---
+++ src/Json.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Liberty\Helpers;
+
+use stdClass;
+use Liberty\Cache\Cache;
+use Liberty\Cache\Settings;
+use Liberty\FileSystem\File;
+
+/**
+ * Класс Json
+ * @version 0.0.1
+ * @package Liberty\Helpers
+ * @generated Зорин Алексей, please DO NOT EDIT!
+ * @author Зорин Алексей <zorinalexey59292@gmail.com>
+ * @copyright 2022 разработчик Зорин Алексей Евгеньевич.
+ */
+final class Json
+{
+
+}
---
+++ src/polifil.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( ! defined('SEP')) {
+    define('SEP', DIRECTORY_SEPARATOR);
+}
+
+
+if ( ! defined('TMP')) {
+    define('ROOT', __DIR__ . SEP);
+}
+
+if ( ! defined('TMP')) {
+    define('TMP', ROOT . 'TMP' . SEP);
+}
